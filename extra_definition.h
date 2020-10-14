8a74a41c3f9af3e70b71aa8c11576319a6d3b390
JerryHe <1072436890@qq.com>
2020-10-14 17:16:55 +0800

    添加项目文件。

---
+++ extra_definition.h
@@ -0,0 +1,12 @@
+#ifndef EXTRA_DEF_H
+#define EXTRA_DEF_H
+
+#include<iostream>
+#include<string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+#endif // !EXTRA_DEF_H
